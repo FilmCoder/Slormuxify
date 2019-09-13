e6a8342981c13fe01a9b73d33a49509cae2305dd
= <dominic.napoleon@gmail.com>
2019-09-13 17:35:56 -0400

    DYNAMIC ENGINE COMPLETE. HOT DAMN SON. close #1

--- chrome_extension/slormux_engine.js
+++ chrome_extension/slormux_engine.js
@@ -1,6 +1,8 @@
 // slormux_engine.js is the content script that runs immediately when you visit
 // a webpage on Chrome.  It converts all text on the page into Slormux.
 
+init();
+
 /**
  * Initial tasks to perform for the content script.  This script has direct
  * access to the webpage's DOM.
@@ -12,52 +14,101 @@ async function init() {
         await Store.set(ENUMS.IS_ON, true);
     }
 
+    // can slormuxify new nodes on the fly
+    dynamicEngine = new DynamicEngine();
+
     // only slormuxify the page if engine is currently on
     if(await Store.get(ENUMS.IS_ON)) {
-        await slormuxifyAllTextNodes();
+        slormuxifyAllTextNodes();
+        dynamicEngine.start();
     }
 
+    // listen for message from popup ui to turn engine on or off
     chrome.runtime.onMessage.addListener(
         function (request, sender, sendResponse) {
             if(request.isEngineOn) {
-                console.log('slormux engine is turning ON');
-                slormuxifyAllTextNodes()
+                slormuxifyAllTextNodes();
+                dynamicEngine.start();
             } 
             else if(request.isEngineOn == false) {
-                console.log('slormux engine is turning OFF')
-                deSlormuxifyAllTextNodes()
+                deSlormuxifyAllTextNodes();
+                dynamicEngine.stop();
             }
         }
     );
 }
-init();
+
+function slormuxifyNode(node) {
+    // don't slormuxify if already slormuxified
+    if(node.is_slormuxified == true) return;
+
+    node.preslormuxify_data = node.data;
+    node.data = slormuxify(node.data);
+    node.is_slormuxified = true;
+}
+
+function deSlormuxifyNode(node) {
+    if(node.is_slormuxified) {
+        node.data = node.preslormuxify_data;
+        node.is_slormuxified = false;
+    }
+}
 
 async function slormuxifyAllTextNodes() {
-    await forAllTextNodes((node) => {
-        node.preslormuxify_data = node.data;
-        node.data = slormuxify(node.data);
+    forTextNodesIn(document, node => {
+        slormuxifyNode(node);
     })
 }
 
 async function deSlormuxifyAllTextNodes() {
-    await forAllTextNodes((node) => {
-        node.data = node.preslormuxify_data;
+    forTextNodesIn(document, node => {
+        deSlormuxifyNode(node);
     })
 }
 
 /**
- * Returns promise that will execute 'func' on all text nodes on the web page.
+ * Applys 'func' on all text nodes under rootNode
+ * @param rootNode Node from which to search for Text nodes
  */
-function forAllTextNodes(func) {
-    return new Promise(resolve => {
-        const allDomElements = document.getElementsByTagName('*');
-        for(element of allDomElements) {
-            for(child of element.childNodes) {
-                if(child.nodeType == Node.TEXT_NODE) {
-                    func(child);
+function forTextNodesIn(rootNode, func) {
+    const allDomElements = rootNode.getElementsByTagName('*');
+    for(element of allDomElements) {
+        for(child of element.childNodes) {
+            if(child.nodeType == Node.TEXT_NODE) {
+                func(child);
+            }
+        }
+    }
+}
+
+/**
+ * Handles dynamic slormuxification of nodes as they appear, for instance on scrolling feeds
+ * like facebook where new text nodes are created on the fly.
+ */
+class DynamicEngine {
+    constructor() {
+        this.config = { childList: true, subtree: true };
+        this.observer = new MutationObserver(this.callback);
+    }
+
+    start() {
+        this.observer.observe(document, this.config);
+    }
+
+    stop() {
+        this.observer.disconnect();
+    }
+
+    // Callback function to execute when mutations are observed
+    callback = function(mutationsList, observer) {
+        for(let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                for(let node of mutation.addedNodes) {
+                    forTextNodesIn(node, textNode => {
+                        slormuxifyNode(textNode);
+                    });
                 }
             }
         }
-        resolve();
-    })
+    };
 }
